a6a7edd0a51c342e8d59001a4926163793512210
Kirill Shamilin <k.shamilin@gmail.com>
2019-01-03 00:59:08 +0300

    Create service.sh

---
+++ common/service.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
+cd /system/vendor/lib/modules
+insmod xt_HL.ko
+
+iptables -t mangle -A POSTROUTING -j TTL --ttl-set 64
