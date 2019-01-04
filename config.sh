c499e353210c7cdc8cc2379264c66264cbbcf37f
Kirill Shamilin <k.shamilin@gmail.com>
2019-01-04 13:11:51 +0300

    Update config.sh
    add device and sdk version check

--- config.sh
+++ config.sh
@@ -21,3 +21,48 @@ set_permissions() {
   set_perm_recursive  $MODPATH  0  0  0755  0644
 }
 
+#------------------------------//////-----------------------------#
+#Checking for supported device. Thx @Didgeridoohan for this function.
+device_fn() {
+# Variables
+DEVFND=0
+DEVICES="
+dipper
+polaris
+perseus
+beryllium
+equuleus
+ursa
+"
+# Device check
+for ITEM in $DEVICES; do
+  if [ $(getprop ro.product.name) == "$(echo $ITEM | cut -f 1 -d '_'))" ] ; then
+    DEVFND=1
+    break
+  fi
+done
+# Abort if no match
+if [ $DEVFND == 0 ]; then
+  abort "Unsupported device or modified buld.prop"
+fi
+}
+
+#Checking SDK version. Thx @Didgeridoohan for this function.
+sdkver_fn() {
+# Variables
+SDKFND=0
+SDKVER="
+28
+"
+# SDK version check
+for ITEM in $SDKVER; do
+  if [ $(getprop ro.build.version.sdk) == "$(echo $ITEM)" ] ; then
+    SDKFND=1
+    break
+  fi
+done
+# Abort if no match
+if [ $SDKFND == 0 ]; then
+  abort "Unsupported Android version or modified build.prop"
+fi
+}
